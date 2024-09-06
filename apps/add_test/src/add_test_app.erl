b5df56c4c3a69af80d2fe9975220e8b39c55d475
joq62 <joq62@c50>
2024-09-06 05:48:40 +0200

    created

---
+++ apps/add_test/src/add_test_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc adder3 public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(add_test_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    add_test_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
